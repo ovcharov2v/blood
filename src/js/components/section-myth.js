830011465e4d93be45c6518cd42d00309e932581
Viktor Ovcharov <ovcharov.2v@gmail.com>
2025-12-09 14:23:47 +0400

    fix: доработки по слайдеру

--- src/js/components/section-myth.js
+++ src/js/components/section-myth.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if (window.innerWidth < 1024 && !swiper) {
       swiper = new Swiper('.section-myth__slider', {
         slidesPerView: 1.3,
-        spaceBetween: 12,
-        slideToClickedSlide: true,
+        spaceBetween: 12,preventClicks: false,
+        preventClicksPropagation: false,
         on: {
           transitionStart: () => {
             slides.forEach((el) => {
@@ -42,8 +42,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const slides = document.querySelectorAll('.section-myth__card')
 
   if(slides.length) {
-    slides.forEach((slide) => {
+    slides.forEach((slide, index) => {
       slide.addEventListener('click', () => {
+        if(swiper) {
+          swiper.slideTo(index)
+        }
         slides.forEach((el) => {
           if(el!==slide) {
             el.classList.remove('section-myth__card--active')
